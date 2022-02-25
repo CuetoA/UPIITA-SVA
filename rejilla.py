80ddde6e36ddd0599aa97409e3f7d500217a6438
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-02-25 10:37:46 -0600

    reduced code

--- rejilla.py
+++ rejilla.py
@@ -1,23 +1,15 @@
 import numpy as np
 import cv2 as cv
 
-ancho = alto = 300
-rejilla = np.ones((alto, ancho), np.uint8) * 255
-rejilla2 = np.ones((alto, ancho, 3), np.uint8) * 255
+color = [88, 24, 183]
+width = high = 300
+image = np.ones((high, width, 3), np.uint8) * 255
 
-# Test
-arreglo = rejilla2[0,0]
-tipo = type(arreglo)
-tamano = len(arreglo)
-size2 = arreglo.size
-print('\narreglo: {} \ntipo: {} \ntamaño: {} \nsize: {}'.format(arreglo,tipo,tamano, size2))
-
-for x in range(ancho):
-    for y in range(alto):
+for x in range(width):
+    for y in range(high):
         if x%50 == 0 or y%50 == 0:
-            rejilla2[x,y][0] = 88
-            rejilla2[x,y][1] = 24
-            rejilla2[x,y][2] = 183
+            pointer = image[x,y]
+            [pointer[0] , pointer[1] , pointer[2]] = color
 
-cv.imshow('Rejilla', rejilla2)
+cv.imshow('Rejilla', image)
 cv.waitKey(0)
