a28b8d6f65c6bf8e02df6b38feff4693d3530dc2
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-03-04 10:23:50 -0600

    noise filter

---
+++ filtros.py
@@ -0,0 +1,27 @@
+'''
+Filtro Maximo con scipy
+'''
+import numpy as np
+import cv2 as cv
+import scipy.ndimage
+
+Img=cv.imread('Lena_Ruido.jpg',1)
+
+'''cv.imshow('Lena Ruido', Img)'''
+img_gray=cv.cvtColor(Img, cv.COLOR_BGR2GRAY)
+Mask=7
+a=scipy.ndimage.maximum_filter(img_gray,size=Mask)
+b=scipy.ndimage.minimum_filter(img_gray,size=Mask)
+c=scipy.ndimage.median_filter(img_gray,size=Mask)
+
+cv.imshow('Lena - Filtro Minimo', b)
+cv.imshow('Lena - Filtro Media', c)
+
+
+cv.namedWindow('Lena - Color',cv.WINDOW_NORMAL)
+cv.moveWindow('Lena - Color', 0,0,)
+cv.arrowedLine(Img,(200,100),(400,300),(255,105,0),4)
+cv.imshow('Lena - Color', Img)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
