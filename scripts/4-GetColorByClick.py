3fc9dc6e6abe219d6d7fe08a95e6c89bc8ad74e2
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-03-25 08:20:37 -0600

    Corrected Get color function

--- scripts/4-GetColorByClick.py
+++ scripts/4-GetColorByClick.py
@@ -3,7 +3,7 @@ import cv2 as cv
 
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname , '../img/Lena2.jpg')
-img = cv.imshow('Color', filename)
+img = cv.imread(filename)
 img_copy = img.copy()
 
 cv.imshow('Color', img_copy)
@@ -12,13 +12,13 @@ def color(event, x, y, flag, param):
     global img
     
     if event == cv.EVENT_LBUTTONDOWN:
-        color = img[x.y] .tolist()
+        color = img[x , y].tolist()
         pixel_radius = 40
         solid = -1
         cv.circle(img, (x,y), pixel_radius, color, solid)
         
-    cv.imshow('Colores', img)
+    cv.imshow('Color', img)
     
-cv.setMouseCallback('Colores', color)
+cv.setMouseCallback('Color', color)
 cv.waitKey(0)
 cv.destroyAllWindows()
