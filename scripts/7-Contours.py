6add0e0227aa8c0e135b91c18ba135eac326f88b
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-04-17 22:38:48 -0400

    Created contour trackbar programm

--- scripts/7-Contours.py
+++ scripts/7-Contours.py
@@ -1,20 +1,30 @@
 import os
 import cv2 as cv
 
-umbral = 200
-color = (0, 255, 255)
+dirname     = os.path.dirname(__file__)
+filename    = os.path.join(dirname , '../img/formas-colores.jpg')
+img         = cv.imread(filename, 1)
+imgCopy     = img.copy()    
+img_gray    = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname , '../img/formas-colores.jpg')
-img = cv.imread(filename, 1)
-img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+def binaryImage(umbral):
+    global img_umbral
+    _,img_umbral = cv.threshold(img_gray, umbral, 255, cv.THRESH_BINARY_INV)
+    cv.imshow("Umbral Image", img_umbral)
 
-_,img_umbral = cv.threshold(img_gray, umbral, 255, cv.THRESH_BINARY_INV)
+def contorno():
+    color       = (0, 255, 255)
+    contorno,_  = cv.findContours(img_umbral, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+    img         = imgCopy.copy()
+    cv.drawContours(img, contorno, -1, color, 2)  
+    cv.imshow("Contours", img)
 
-contorno,_ = cv.findContours(img_umbral, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+def updateImage(umbral):
+    binaryImage(umbral)
+    contorno()
+    
+updateImage(0)
 
-cv.drawContours(img, contorno, -1, color, 2)
-
-cv.imshow('Colores', img)  
+cv.createTrackbar('umbral', "Contours", 0, 255, updateImage)
 cv.waitKey(0)
 cv.destroyAllWindows()
