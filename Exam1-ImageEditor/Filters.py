87a7efecb6e0dd0af0de0874afde543a9d3400ec
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-05-08 09:50:04 -0500

    Added decorator to simplify future functions implementation

--- Exam1-ImageEditor/Filters.py
+++ Exam1-ImageEditor/Filters.py
@@ -1,11 +1,21 @@
 import cv2 as cv
+from PIL import Image
 
 class Filters():
 
     
     def __init__(self):
         self.filtersList = ['Canny']
+        
+    def decorateFunction(func):
+        def wrapper(filename):
+            img = cv.imread(filename)
+            img = func(filename)
+            return Image.fromarray(img)
+            
+        return wrapper
     
+    @decorateFunction
     def canny(filename):
         img = cv.imread(filename)
         img_canny = cv.Canny(img, 50, 200, None, 3)
--- Exam1-ImageEditor/ImageEditorApp.py
+++ Exam1-ImageEditor/ImageEditorApp.py
@@ -8,6 +8,16 @@ from Filters import Filters
 window = sg.Window("Cueto's Image Editor", layout())
 filters = Filters
 
+
+
+
+def printImage():
+    image.thumbnail((400, 400))
+    bio = io.BytesIO()
+    image.save(bio, format="PNG")
+    window["-IMAGE-"].update(data=bio.getvalue())
+
+
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
@@ -16,19 +26,14 @@ while True:
         filename = values["-FILE-"]
         if os.path.exists(filename):
             image = Image.open(values["-FILE-"])
-            image.thumbnail((400, 400))
-            bio = io.BytesIO()
-            image.save(bio, format="PNG")
-            window["-IMAGE-"].update(data=bio.getvalue())
+            printImage()
+            
     if values['-FILTERS-']:
         print(f"Selected value: {values['-FILTERS-']}")
         if values['-FILTERS-'] == 'Canny':
             image = filters.canny(filename)
-            image = Image.fromarray(image)
-            image.thumbnail((400, 400))
-            bio = io.BytesIO()
-            image.save(bio, format="PNG")
-            window["-IMAGE-"].update(data=bio.getvalue())
+            printImage()
+            
 
 
 window.close()
