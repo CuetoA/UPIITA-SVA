b59582323f548025fe5ec44645e4ee6543552139
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-05-08 08:21:16 -0500

    Created toogle list with events

--- Exam1-ImageEditor/ImageEditorApp.py
+++ Exam1-ImageEditor/ImageEditorApp.py
@@ -3,30 +3,35 @@ import os
 import PySimpleGUI as sg
 from PIL import Image
 
+mylist = ['Scarlette', 'Andrés', 'Boni']
+
 file_types = [("JPEG (*.jpg)", "*.jpg"),
               ("All files (*.*)", "*.*")]
 
-colum1 = [
+column1 = [
     [sg.Image(key="-IMAGE-")],
 ]
 
-colum2 = [
+column2 = [
     [
         sg.Text("Image File"),
         sg.Input(size=(30, 1), key="-FILE-"),
         sg.FileBrowse(file_types=file_types),
         sg.Button("Load Image"),
-    ]
+    ],
+    [sg.Combo(mylist, enable_events=True, key='-FILTERS-')],
 ]
 
 layout = [
     [sg.Text("Welcome, select a file")],
-    [sg.Column(colum1),
-     sg.Column(colum2),
-     ]
+    [sg.Column(column1),
+     sg.Column(column2, key="-COLUMN2-")]
 ]
 
 window = sg.Window("Cueto's Image Editor", layout)
+
+
+
 
 while True:
     event, values = window.read()
@@ -40,6 +45,8 @@ while True:
             bio = io.BytesIO()
             image.save(bio, format="PNG")
             window["-IMAGE-"].update(data=bio.getvalue())
-        
+    if values['-FILTERS-']:
+        print(f"Selected value: {values['-FILTERS-']}")
+
 
 window.close()
