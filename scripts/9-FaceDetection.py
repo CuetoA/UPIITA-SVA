5f550e4b384cfe37c98e4ae9869e020bbaa722ee
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-05-09 07:37:12 -0500

    face detector created

---
+++ scripts/9-FaceDetection.py
@@ -0,0 +1,22 @@
+import os
+import cv2 as cv
+
+color = 0
+grosor = 2
+escala = 1
+
+
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname , '../img/Lena2.jpg')
+img = cv.imread(filename)
+
+clasficador = cv.CascadeClassifier('base de datos que no descargué')
+grayImg = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+caras = clasficador.detectMultiscale(grayImg)
+
+for (x, y, ancho, alto) in caras:
+    cv.rectangle(img, (x,y), (x+ancho, y+alto), color, grosor)
+    
+cv.imshow('Rostros', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
