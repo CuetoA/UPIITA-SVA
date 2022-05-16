613997db129d34f811e5fe2f4fac39d543259eea
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-05-15 19:46:27 -0500

    read image succesfully

--- scripts/9-FacialRecognition/main.py
+++ scripts/9-FacialRecognition/main.py
@@ -7,7 +7,7 @@ import face_recognition
 curentPath = os.getcwd()
 completePath = f"{curentPath}/scripts/9-FacialRecognition/"
 img1 = completePath + 'knownFacesDir/Andres/andres1.jpg'
-img2 = np.copy(img1)
+img2 = img1
 # knownFacesDir = completePath + "knownFacesDir"
 # unknownFacesDir = completePath + "unknownFacesDir/this"
 tolerance = 0.8
@@ -20,5 +20,19 @@ andres_image = face_recognition.load_image_file(img1)
 andres_facencoding = face_recognition.face_encodings(andres_image)[0]
 
 
+known_face_encodings = [
+    andres_facencoding,
+]
+known_face_names = [
+    "La Cuetorra",
+]
 
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
 
+img = cv.imread(img2)
+cv.imshow('test', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
