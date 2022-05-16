38b27c52b5b42677901a15ac108b923c61a77d2e
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-05-15 19:52:08 -0500

    Found first face

--- scripts/9-FacialRecognition/main.py
+++ scripts/9-FacialRecognition/main.py
@@ -33,6 +33,35 @@ face_names = []
 process_this_frame = True
 
 img = cv.imread(img2)
-cv.imshow('test', img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+# Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+img = img[:, :, ::-1]
+
+face_locations = face_recognition.face_locations(img)
+face_encodings = face_recognition.face_encodings(img, face_locations)
+
+
+
+face_names = []
+name = "Unknown"
+for face_encoding in face_encodings:
+    # See if the face is a match for the known face(s)
+    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+    
+    # # If a match was found in known_face_encodings, just use the first one.
+    # if True in matches:
+    #     first_match_index = matches.index(True)
+    #     name = known_face_names[first_match_index]
+
+    # Or instead, use the known face with the smallest distance to the new face
+    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+    best_match_index = np.argmin(face_distances)
+    if matches[best_match_index]:
+        name = known_face_names[best_match_index]
+        
+
+    face_names.append(name)
+
+
+# cv.imshow('test', img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
